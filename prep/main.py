2cc474f7335a4892be0609aedc8a02590309b3d7
EduRibeiro00 <eribeiro306@gmail.com>
2021-02-15 14:48:45 +0000

    Main now receives filename from argv

--- prep/main.py
+++ prep/main.py
@@ -1,7 +1,11 @@
 from files.input import parse_input
 from files.output import output_to_filename
-
+import sys
 
 if __name__ == '__main__':
-    state = parse_input('i.txt')
+    if len(sys.argv) != 2:
+        print("Specify the input file in the arguments")
+        sys.exit(-1)
+    
+    state = parse_input(sys.argv[1])
     output_to_filename(state, 'o.txt')
