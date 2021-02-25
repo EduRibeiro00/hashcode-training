6928b179372315f0fc745854945577f7a7547de2
daviddias99 <up201705373@fe.up.pt>
2021-02-25 18:55:06 +0000

    First solve attempt

--- online-qualification-round/files.py
+++ online-qualification-round/files.py
@@ -11,23 +11,24 @@ def read_input(problem):
 def parse_input(lines):
     dur, n_intersections, n_streets, n_cars, n_bonus = [int(n) for n in lines[0].split(' ')]
 
-    streets = []
+    streets = {}
     for line in lines[1:n_streets+1]:
         int_start, int_end, name, time = [e for e in line.split(' ')]
         street = {
             'int_start': int(int_start),
             'int_end': int(int_end),
-            'name': name,
-            'time': int(time)
+            'time': int(time),
+            'name': name
         }
-        streets.append(street)
+
+        streets[name] =street
 
     cars = []
     for line in lines[1+n_streets:]:
         split_line = line.split(' ')
         car = {
             'n_streets': int(split_line[0]),
-            'path': split_line[1:]
+            'path': split_line[1:-1]
         }
         cars.append(car)
 
--- online-qualification-round/main.py
+++ online-qualification-round/main.py
@@ -14,6 +14,31 @@ def main(args):
 
     dur, n_intersections, n_streets, n_cars, n_bonus, streets, cars = parse_input(lines)
 
+    # distances = [(id, sum([streets[edge]['time'] for edge in car['path'] ])) for id, car  in enumerate(cars)]
+    # distances.sort(key= lambda x: x[1]) 
+
+    street_car_count = {}
+
+    for car in cars:
+        for street_name in car['path']:
+            if not streets[street_name]:
+                street_car_count[street_name] = 0
+            street_car_count[street_name] += 1
+
+    intersections = {}
+
+    for i in range(n_intersections):
+
+        semaphores = []
+
+        for street_name in streets:
+            if streets[street_name]['int_end'] == i:
+                if street_car_count[street_name] != 0:
+                    semaphores.append((street_name, street_car_count[street_name])) 
+
+        intersections[i] = semaphores
+
+
     write_output(problem)
 
 if __name__ == '__main__':
