83e99f433b9be310f4d11a3c56804cf00c2f9df8
luispcunha <luispplmcunha@gmail.com>
2021-02-25 09:57:54 +0000

    Add initial files for online qualification round

---
+++ online-qualification-round/files.py
@@ -0,0 +1,24 @@
+import os
+
+def read_input(problem):
+    in_path = os.path.join(os.path.dirname(__file__), 'input', f'{problem}')
+
+    with open(in_path) as f:
+        input = f.readlines()
+
+    return input
+
+def parse_input(input):
+    # TODO: parse input
+    pass
+
+
+def write_output(problem):
+    out_dir = os.path.join(os.path.dirname(__file__), 'output')
+    os.makedirs(out_dir, exist_ok=True)
+    out_path = os.path.join(out_dir, f'{problem}.out')
+
+    with open(out_path, 'w') as out:
+        # TODO: write output
+        out.write('hi')
+        pass
---
+++ online-qualification-round/main.py
@@ -0,0 +1,22 @@
+import os
+import sys
+from files import read_input, parse_input, write_output
+
+
+def main(args):
+    if len(args) != 1:
+        print('One argument expected: id of problem to solve (a, b, c, ...)')
+        sys.exit(-1)
+
+    problem = args[0]
+
+    read_input(problem)
+
+    parse_input(input)
+
+
+
+    write_output(problem)
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
